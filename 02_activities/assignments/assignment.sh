98100a3acc1d285a65b70a9bcf80d51059a298d4
Parisa Ahmadighotbi <164830791+Parisaghotbi@users.noreply.github.com>
2024-08-22 15:38:56 -0400

    Assignment.sh: 6 Questions got answered
    The answers should be checked on git bash

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -9,15 +9,26 @@ set -x
 
 # 1. Create 5 directories named dir1, dir2, dir3, dir4, and dir5
 mkdir dir1 dir2 dir3 dir4 dir5
+
 # 2. List the contents of the parent directory to verify the presence of the 5 directories
+ls
 
 # 3. Create 5 text files in dir2 named file1, file2, file3, file4, and file5
+touch dir2/file1.txt
+touch dir2/file2.txt
+touch dir2/file3.txt
+touch dir2/file4.txt
+touch dir2/file5.txt
 
 # 4. Append the words "Hello world" to dir2/file3
+echo "Hello world" >> dir2/file3.txt
 
 # 5. Verify that file3 contains the words "hello world" by printing the contents of the file in the terminal
+cat dir2/file3.txt
 
 # 6. Delete file4
+cd dir2
+rm file4.txt
 
 # 7. Delete directories dir4 and dir5 including all their contents (if any)
 
